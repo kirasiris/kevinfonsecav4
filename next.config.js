caa918ee06458862bd2f8655c2024e472ba75b2d
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2024-08-31 21:14:28 -0500

    CORS issue fixed?

--- next.config.js
+++ next.config.js
@@ -27,14 +27,26 @@ const nextConfig = {
 			},
 		],
 	},
-	// headers: async () => {
-	// 	return [
-	// 		{
-	// 			source: "/:path*{/}?",
-	// 			headers: [{}],
-	// 		},
-	// 	];
-	// },
+	headers: async () => {
+		return [
+			{
+				source: "/:path*{/}?",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,HEAD,POST,PUT,DELETE,CONNECT,OPTIONS,TRACE,PATCH",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value:
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;

0c9811b637cfb2122d4449680784f8590d1953a2
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2025-10-23 11:55:16 -0500

    fixed single theme object

--- components/theme/single.js
+++ components/theme/single.js
@@ -17,10 +17,10 @@ const Single = ({ object = {} }) => {
 							object?.files?.avatar?.location?.secure_location ||
 							`https://source.unsplash.com/random/1200x900`
 						}
-						className="card-img-top div-hover"
+						className="img-fluid"
 						alt={`${object?.title || "Untitled"}'s featured image`}
-						width={`850`}
-						height={`350`}
+						width={`1200`}
+						height={`900`}
 					/>
 				</Link>
 			</article>

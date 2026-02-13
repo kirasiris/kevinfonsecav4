4a8775dea11a8e20797917946ca33f4d01f31ee6
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2026-02-12 19:38:57 -0600

    Removed jobs from this project. Jobs can be found on Armed Code, LLC

--- app/rss/page.js
+++ app/rss/page.js
@@ -36,7 +36,7 @@ const RSSIndex = async ({ params, searchParams }) => {
 			/>
 			<div className="container">
 				<div className="row">
-					<div className="col">
+					<div className="col-lg-3">
 						<div className="card mb-3">
 							<div className="card-header">Blogs</div>
 							<ul className="list-group list-group-flush">
@@ -71,7 +71,7 @@ const RSSIndex = async ({ params, searchParams }) => {
 							</ul>
 						</div>
 					</div>
-					<div className="col">
+					<div className="col-lg-3">
 						<div className="card mb-3">
 							<div className="card-header">Themes</div>
 							<ul className="list-group list-group-flush">
@@ -106,7 +106,7 @@ const RSSIndex = async ({ params, searchParams }) => {
 							</ul>
 						</div>
 					</div>
-					<div className="col">
+					<div className="col-lg-3">
 						<div className="card mb-3">
 							<div className="card-header">Secrets</div>
 							<ul className="list-group list-group-flush">
@@ -141,7 +141,7 @@ const RSSIndex = async ({ params, searchParams }) => {
 							</ul>
 						</div>
 					</div>
-					<div className="col">
+					<div className="col-lg-3">
 						<div className="card mb-3">
 							<div className="card-header">Forum</div>
 							<ul className="list-group list-group-flush">
@@ -164,41 +164,6 @@ const RSSIndex = async ({ params, searchParams }) => {
 									<Link
 										href={{
 											pathname: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/forum/feed.xml`,
-											query: {},
-										}}
-										className="btn btn-outline-secondary btn-sm"
-										rel="noreferrer"
-										target="_blank"
-									>
-										<MdRssFeed color="#ee802f" size="25px" />
-									</Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div className="col">
-						<div className="card mb-3">
-							<div className="card-header">Jobs</div>
-							<ul className="list-group list-group-flush">
-								<li className="list-group-item">
-									Sitemap:&nbsp;
-									<Link
-										href={{
-											pathname: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/job/sitemap.xml`,
-											query: {},
-										}}
-										className="btn btn-outline-secondary btn-sm"
-										rel="noreferrer"
-										target="_blank"
-									>
-										<MdRssFeed color="#ee802f" size="25px" />
-									</Link>
-								</li>
-								<li className="list-group-item">
-									Feed:&nbsp;
-									<Link
-										href={{
-											pathname: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/job/feed.xml`,
 											query: {},
 										}}
 										className="btn btn-outline-secondary btn-sm"

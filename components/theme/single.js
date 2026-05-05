b5eb0b4bc92475a7ac3c6443ef76b8e5b063f8c4
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2026-05-04 22:02:19 -0500

    Restored single theme component to be used in main page

---
+++ components/theme/single.js
@@ -0,0 +1,32 @@
+"use client";
+import { Suspense } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Loading from "@/app/theme/loading";
+
+const Single = ({ object = {} }) => {
+	return (
+		<Suspense fallback={<Loading />}>
+			<article className={`${object?._id} col-lg-4 mb-4`}>
+				<Link
+					href={{
+						pathname: `${process.env.NEXT_PUBLIC_ARMED_CODELLC_URL}/${object?._id}/${object?.category?._id}/${object?.category?.slug}/${object?.slug}`,
+					}}
+				>
+					<Image
+						src={
+							object?.files?.avatar?.location?.secure_location ||
+							`https://picsum.photos/1200/900?blur`
+						}
+						className="img-thumbnail"
+						alt={`${object?.title || "Untitled"}'s featured image`}
+						width={`1200`}
+						height={`900`}
+					/>
+				</Link>
+			</article>
+		</Suspense>
+	);
+};
+
+export default Single;

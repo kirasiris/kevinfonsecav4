a21983401212d6fe978343b3e8bd4dad6b6712d1
Kevin Uriel Azuara Fonseca <kebin1421@hotmail.com>
2026-09-19 01:31:31 -0500

    Removed logo image from contact form page

--- layout/contact/sidebar.js
+++ layout/contact/sidebar.js
@@ -1,17 +1,10 @@
 "use client";
-import Image from "next/image";
 import Map from "@/components/global/map";
 import Globalsidebar from "../sidebar";
 
 const Sidebar = ({ object = {} }) => {
 	return (
 		<Globalsidebar>
-			<Image
-				src={object?.data?.logo?.location?.secure_location}
-				width={150}
-				height={40}
-				alt={`${object?.data?.title}'s logo`}
-			/>
 			<div className="card">
 				<div className="card-header">Address</div>
 				<div className="card-body">
